4d20075f34fdcc691dae30b5a84d75e556e6a9f1
Žymantas Gedminas <zymantas.gedminas@shopify.com>
2024-11-27 10:50:07 +0200

    Fix regex for running test classes with nested modules

--- lua/neotest-minitest/init.lua
+++ lua/neotest-minitest/init.lua
@@ -105,7 +105,7 @@ function NeotestAdapter.build_spec(args)
     table.insert(script_args, spec_path)
     table.insert(script_args, "--name")
     -- https://chriskottom.com/articles/command-line-flags-for-minitest-in-the-raw/
-    table.insert(script_args, "/^" .. full_name .. "$/")
+    table.insert(script_args, "/" .. full_name .. "/")
   end
 
   local function run_dir()
